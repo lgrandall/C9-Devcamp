63ef0b737e5aa3d24aa3616856bcd7cc87752d07
Louis Randall <lgrandall92@gmail.com>
2017-03-08 19:56:45 -0800

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/about'
+
+  get 'home/contact'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
