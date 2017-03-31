717337060e92a2d0b3b8141ceb0306b5f69c8e3f
Louis Randall <lgrandall92@gmail.com>
2017-03-30 19:10:07 -0700

    implemented default imgaes for portfolio features

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,11 @@ end
 	def self.ruby_on_rails
 		where(subtitle: 'Ruby on Rails')
 	end
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
